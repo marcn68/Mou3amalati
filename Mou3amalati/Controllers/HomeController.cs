f85671f35f2e222f9d1ae27fb398322f11b9944c
Marc Nehme <201610021@ua.edu.lb>
2020-12-01 21:35:28 +0200

    T250-Added RequestAccount Page with design

--- Mou3amalati/Controllers/HomeController.cs
+++ Mou3amalati/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mou3amalati.Models;
@@ -23,6 +24,11 @@ namespace Mou3amalati.Controllers
             return View();
         }
 
+        public IActionResult RequestAccount()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -32,6 +38,15 @@ namespace Mou3amalati.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SubmitAccount(FormCollection RequestAccount)
+        {
+            string id = RequestAccount["identity"];
+            string email = RequestAccount["email"];
+
+
+            return View();
         }
     }
 }
