f521f3b98febab979a3ccdabd1136abdcfe74fc3
Marc Nehme <201610021@ua.edu.lb>
2020-12-04 21:32:55 +0200

    Added functionalities to the request account page

--- Mou3amalati/Controllers/HomeController.cs
+++ Mou3amalati/Controllers/HomeController.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mou3amalati.Models;
+using Mou3amalati.BLL;
+using Mou3amalati.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace Mou3amalati.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        public UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -40,13 +46,37 @@ namespace Mou3amalati.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult SubmitAccount(FormCollection RequestAccount)
+        [HttpPost]
+        public async Task<IActionResult> SubmitAccountAsync(IFormCollection RequestAccount)
         {
+            HomeManager home = new HomeManager();
             string id = RequestAccount["identity"];
             string email = RequestAccount["email"];
+            string password = home.CreateRandomPassword(10);
 
+            var citizen = await _context.Citizens.FindAsync(id);
+            //var citizen = _context.Citizens.Find(id);
 
-            return View();
+            if(citizen != null)
+            {
+                if (citizen.IdentityUser.Id != null)
+                {
+                    var user = new IdentityUser { UserName = citizen.IdentityUser.Email, Email = citizen.IdentityUser.Email };
+                    var result = await _userManager.CreateAsync(user, citizen.IdentityUser.PasswordHash);
+                    user.EmailConfirmed = true;
+                    citizen.IdentityUser = null;
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User created a new account with password.");
+                        citizen.IdentityUser = user;
+                        _context.Users.Add(user);
+                        await _context.SaveChangesAsync();
+                    }
+                    home.EmailSender(id, email, password);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
