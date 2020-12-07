98e3aea19184f1ca90a9a3ee9eca43749d57d198
Marc Nehme <201610021@ua.edu.lb>
2020-12-07 22:04:38 +0200

    Added documents controller

---
+++ Mou3amalati/Controllers/DocumentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mou3amalati.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
