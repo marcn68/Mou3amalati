17fecaadd7497685225ac1c50221bf260dd89ed5
Marc Nehme <201610021@ua.edu.lb>
2020-12-08 20:58:35 +0200

    Added the Ekhraj kayd page

--- Mou3amalati/Controllers/DocumentsController.cs
+++ Mou3amalati/Controllers/DocumentsController.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Mou3amalati.Data;
+using Mou3amalati.Models;
 
 namespace Mou3amalati.Controllers
 {
     public class DocumentsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DocumentsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult PersonalStatusRecord(Citizen c)
+        {
+            var userName = User.FindFirstValue(ClaimTypes.Name);
+
+            c = _context.Citizens.Find(userName); 
+
+            return View(c);
+        }
+
+        public IActionResult Assigned()
         {
             return View();
         }
