df80980ceb62d4f7505d9cabdd908a0a467a7f31
Marc Nehme <201610021@ua.edu.lb>
2020-12-20 21:48:14 +0200

    Added features to add records of tables to the db after finishing the request of a document

--- Mou3amalati/Controllers/DocumentsController.cs
+++ Mou3amalati/Controllers/DocumentsController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Mou3amalati.Data;
 using Mou3amalati.Models;
+using Mou3amalati.ViewModel;
 
 namespace Mou3amalati.Controllers
 {
@@ -15,6 +16,7 @@ namespace Mou3amalati.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
+        private string assignedToValue;
 
         public DocumentsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
@@ -33,8 +35,9 @@ namespace Mou3amalati.Controllers
 
         public async Task<IActionResult> Assigned()
         {
-            var userName = User.FindFirstValue(ClaimTypes.Name);
+            DocumentsAssignedViewModel docVM = new DocumentsAssignedViewModel();
 
+            var userName = User.FindFirstValue(ClaimTypes.Name);
             Citizen c = _context.Citizens.Find(userName);
 
             var usersOfRole = await _userManager.GetUsersInRoleAsync("Mokhtar");
@@ -42,7 +45,7 @@ namespace Mou3amalati.Controllers
             foreach(var u in usersOfRole)
             {
                 string username = u.UserName;
-                SelectList mokhtarList = new SelectList(
+                docVM.roleList = new SelectList(
                 _context.Citizens.Where(
                 citizen => citizen.OriginAddress.City == c.OriginAddress.City && citizen.ApplicationIdentityUser.UserName == username )
                 .Select(a => new
@@ -52,11 +55,56 @@ namespace Mou3amalati.Controllers
                 }), "CitizenFName", "CitizenLName");
             }
 
-            return View();
+            return View(docVM);
+        }
+
+        [HttpPost]
+        public IActionResult Assigned(DocumentsAssignedViewModel docVM)
+        {
+            assignedToValue = Request.Form["roleList"].ToString();
+
+            return View(docVM);
         }
 
         public IActionResult RequestFinished()
         {
+            var userName = User.FindFirstValue(ClaimTypes.Name);
+            Citizen c = _context.Citizens.Find(userName);
+
+            DateTime nowDate = DateTime.Now;
+
+            Document doc = (Document)_context.Documents.Where(d => d.Name == "Personal Status Record");
+
+            ApplicationIdentityRole role = (ApplicationIdentityRole) _context.Roles.Where(d => d.Name == "Mokhtar");
+
+            WorkFlow wf = _context.WorkFlows.Where(w => w.DocumentId == doc.Id && w.RoleId == role.Id).First();
+
+            DocumentRequest docRequest = new DocumentRequest()
+            {
+                RequestDate = nowDate,
+                CurrentOrdinalPositionInWorkflow = wf.OrdinalPosition,
+                RequestedByCitizenId = c.Id,
+                CurrentAssignedToCitizenId = assignedToValue,
+                DocumentId = doc.Id
+            };
+
+            _context.DocumentRequests.Add(docRequest);
+
+            int docRequestId = _context.DocumentRequests.Where(d => d == docRequest).Select(c => c.Id).FirstOrDefault();
+
+            DocumentStatus docStatus = _context.DocumentStatuses.Find(1);
+            DocumentRequestStatus docRequestStatus = new DocumentRequestStatus()
+            {
+                StatusDate = nowDate,
+                DocumentRequestId = docRequestId,
+                StatusId = docStatus.Id,
+                CitizenId = assignedToValue
+            };
+
+            _context.DocumentRequestStatuses.Add(docRequestStatus);
+
+            _context.SaveChanges();
+            
             return View();
         }
     }
