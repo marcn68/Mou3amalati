10de8697d8c5462de1c6bdbf8bb5a71231568e3a
Marc Nehme <201610021@ua.edu.lb>
2020-12-23 02:20:28 +0200

    Added DocumentId to the ViewModel

--- Mou3amalati/ViewModels/DocumentsAssignedViewModel.cs
+++ Mou3amalati/ViewModels/DocumentsAssignedViewModel.cs
@@ -9,11 +9,10 @@ namespace Mou3amalati.ViewModels
     public class DocumentsAssignedViewModel
     {
         public SelectList roleList { get; set; }
-        public string SelectedRoleCitizen { get; set; }
-        public string SelectedText { get; set; }
         public string RoleName { get; set; }
         public string SelectedRoleCitizenId { get; set; }
         public int DocRequestId { get; set; }
         public bool isLast { get; set; } = false;
+        public int DocumentId { get; set; }
     }
 }
